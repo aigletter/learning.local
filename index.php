63d2be933f8d0b9c4b7631df972f6bafde07b883
Yurii Orlyk <aigletter@gmail.com>
2023-01-02 22:24:00 +0200

    index

--- index.php
+++ index.php
@@ -1,6 +1,57 @@
 <?php
 
-include __DIR__ . '/oop/Dog.php';
+ini_set('display_errors', '1');
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$application = new \Learning\Local\test\boo\Application();
+$application->run();
+
+$order = new \Learning\Local\shop\cart\Order();
+$order->foo();
+
+
+$math = new \Learning\Math\Math();
+$result = $math->sum(5, 10);
+
+echo $result . PHP_EOL;
+
+
+
+
+
+
+
+
+
+/*$client = new \GuzzleHttp\Client();
+$response = $client->request('GET', 'https://google.com');
+
+$html = $response->getBody()->getContents();
+$html = str_replace('src="/', 'src="https://www.google.com/', $html);
+
+echo $html;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*include __DIR__ . '/oop/Dog.php';
 
 $dog1 = new Dog('Bob', 2);
 $dog1->sayHello();
@@ -23,7 +74,7 @@ $dog3->name = 'Tuzik';
 
 //echo $dog1->name;
 $result2 = $dog1 === $dog2;
-var_dump($result2);
+var_dump($result2);*/
 
 
 /*$dog5 = new Dog('Bobik');
